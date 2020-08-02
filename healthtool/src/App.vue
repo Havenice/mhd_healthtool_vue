411936791409200a640476800b8b0b3f1cfc83b6
SayChunKim <saychunkim@gmail.com>
2020-08-02 16:17:48 +0800

    First Commit

---
+++ healthtool/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <router-link to="/bmi-check">BMI</router-link>
+    </div> -->
+  <HealthtoolNavbar/>
+  <div class="container" id="content">
+    <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+import HealthtoolNavbar from './components/HealthtoolNavbar'
+export default {
+  components: { HealthtoolNavbar }
+}
+</script>
+<style>
+  body{
+    background:#f2f2f2;
+  }
+  #content{
+    margin-top:2rem;
+  }
+</style>
+<style lang="scss">
+@import "~bulma/sass/utilities/_all";
+
+// Set your colors
+$primary: #ff9800;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099FF;
+$twitter-invert: findColorInvert($twitter);
+
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+    "white": ($white, $black),
+    "black": ($black, $white),
+    "light": ($light, $light-invert),
+    "dark": ($dark, $dark-invert),
+    "primary": ($primary, $primary-invert),
+    "info": ($info, $info-invert),
+    "success": ($success, $success-invert),
+    "warning": ($warning, $warning-invert),
+    "danger": ($danger, $danger-invert),
+    "twitter": ($twitter, $twitter-invert)
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+</style>
---
+++ healthtool/src/views/Home.vue
@@ -0,0 +1,80 @@
+<template>
+<div class="columns">
+  <div class="box column">
+  <article class="media">
+    <div class="media-left">
+      <figure class="image is-256x256">
+        <img src="https://bulma.io/images/placeholders/256x256.png" alt="Image">
+      </figure>
+    </div>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+          <br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+        </p>
+      </div>
+    </div>
+  </article>
+</div>
+<div class="box column">
+  <article class="media">
+    <div class="media-left">
+      <figure class="image is-256x256">
+        <img src="https://bulma.io/images/placeholders/256x256.png" alt="Image">
+      </figure>
+    </div>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+          <br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+        </p>
+      </div>
+    </div>
+  </article>
+</div>
+<div class="box column">
+  <article class="media">
+    <div class="media-left">
+      <figure class="image is-256x256">
+        <img src="https://bulma.io/images/placeholders/256x256.png" alt="Image">
+      </figure>
+    </div>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+          <br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+        </p>
+      </div>
+    </div>
+  </article>
+</div>
+<div class="box column">
+  <article class="media">
+    <div class="media-left">
+      <figure class="image is-256x256">
+        <img src="https://bulma.io/images/placeholders/256x256.png" alt="Image">
+      </figure>
+    </div>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+          <br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+        </p>
+      </div>
+    </div>
+  </article>
+</div>
+
+</div>
+</template>
+<style scoped>
+
+</style>
